df1f3f081d4a21ee775720e21d418373858cde5b
David Eisner <david.eisner@oriel.oxon.org>
2011-11-15 18:23:32 +0000

    Version number includes git description

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,2 +1,12 @@
+def git_description():
+	import subprocess
+	ps = subprocess.Popen(
+	 ["git", "describe", "--always"],
+	 stdout=subprocess.PIPE,
+	 universal_newlines=True)
+	(out, err) = ps.communicate()
+	return out.strip()
 def get_titanium_version():
-	return '1.2.0.RC3'
+	d=git_description()
+	v='1.2.0'
+	return d if d.startswith(v) else v + ("" if len(d) == 0 or d[0] in ".-_~:" else ".") + d
