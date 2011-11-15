5d37c02296ffbf563985d551916a89461d7c8352
David Eisner <david.eisner@oriel.oxon.org>
2011-11-15 18:23:32 +0000

    Version number includes git description
    Conflicts:
    
    	sdk/__init__.py

--- sdk/__init__.py
+++ sdk/__init__.py
@@ -1,2 +1,14 @@
+import itertools
+def git_description():
+	import subprocess
+	ps = subprocess.Popen(
+	 ["git", "describe", "--always"],
+	 stdout=subprocess.PIPE,
+	 universal_newlines=True)
+	(out, err) = ps.communicate()
+	return out.strip()
 def get_titanium_version():
-	return '1.2.0.RC6e'
+	d=git_description()
+	v='1.2.0'
+	d=list(itertools.islice(itertools.chain((d[:-len(i)] for i in map("".join, itertools.product("-.:~",["win","w","osx","mac","linux","lin","l","unix","dar","os2","os2-"],["","32","64"])) if d.endswith(i)), [d]), 1))[0]
+	return d if d.startswith(v) else v + ("" if len(d) == 0 or d[0] in ".-_~:" else ".") + d
